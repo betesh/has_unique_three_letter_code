8063baa25c1e17ce21ff37a1a3988b762293bd86
Isaac Betesh <ibetesh@springbig.com>
2014-01-23 15:15:05 -0500

    applied the class method in test model with some basic configuration options

--- spec/support/model.rb
+++ spec/support/model.rb
@@ -1,3 +1,9 @@
 class Client < ActiveRecord::Base
   attr_accessible :tla, :name, :needs_tla, :tla_group
+
+  include HasUniqueThreeLetterCode
+
+  has_unique_three_letter_code :tla, :source => :name,
+    :uniqueness => { :scope => :tla_group },
+    :leave_blank => { :unless => :needs_tla }
 end
